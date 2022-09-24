45ebd5ca65b74dfbb6938e043a7b82cf28b80ea2
jackma <1427113689@qq.com>
2022-09-24 16:58:57 +0800

    deploy

---
+++ precache-manifest.d022363f0b94a724c1c2c0673fc8f277.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f785bf1e05c8e3a73f14",
+    "url": "/bill-website/css/app.03bdb5ce.css"
+  },
+  {
+    "revision": "33ef9984faaa59a61fca446797989b17",
+    "url": "/bill-website/index.html"
+  },
+  {
+    "revision": "f785bf1e05c8e3a73f14",
+    "url": "/bill-website/js/app.3b6ace28.js"
+  },
+  {
+    "revision": "35b5edc225185afb216f",
+    "url": "/bill-website/js/chunk-vendors.7e2839c2.js"
+  },
+  {
+    "revision": "1a824f6123383106b5520c628fa7a7bc",
+    "url": "/bill-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/bill-website/robots.txt"
+  }
+]);
